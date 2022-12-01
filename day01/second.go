0cd98b1f28f058acde87e1472173c05b70aa2f02
Charles Parmentier <32079518+Cpavrai@users.noreply.github.com>
2022-12-01 23:12:58 +0100

    Day 01: 2/2

---
+++ day01/second.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "strconv"
+    "sort"
+    "os"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func compute(elve string)(int) {
+    result := 0
+
+    for _, value := range strings.Split(elve, "\n") {
+        intVar, err := strconv.Atoi(value)
+        if (err != nil) {
+            fmt.Print(err)
+            return 0
+        }
+        result += intVar
+    }
+    return result
+}
+
+func getThreeBiggest(elves []string)(int) {
+    sort.SliceStable(elves, func(i, j int) bool {
+        return compute(elves[i]) > compute(elves[j])
+    })
+
+    return compute(elves[0]) + compute(elves[1]) + compute(elves[2])
+}
+
+func main() {
+    if (len(os.Args) == 2) {
+        dat, err := os.ReadFile(os.Args[1])
+        check(err)
+        chars := strings.Split(string(dat), "\n\n")
+        if (len(chars) == 0) {
+            fmt.Print("File empty or unvalid")
+        } else {
+            fmt.Println(getThreeBiggest(chars))
+        }
+    } else {
+        fmt.Print("Error, you need to pass a file as argument.")
+    }
+}
