7ec6e90f094cd840bb8a94a007311060424c5ded
Cpavrai <32079518+Cpavrai@users.noreply.github.com>
2022-12-02 23:24:28 +0100

    Global: Reformated all code

--- day01/first.go
+++ day01/first.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "strconv"
-    "os"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func compute(elve string)(int) {
-    result := 0
+func compute(elve string) int {
+	result := 0
 
-    for _, value := range strings.Split(elve, "\n") {
-        intVar, err := strconv.Atoi(value)
-        if (err != nil) {
-            fmt.Println(err)
-            return 0
-        }
-        result += intVar
-    }
-    return result
+	for _, value := range strings.Split(elve, "\n") {
+		intVar, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Println(err)
+			return 0
+		}
+		result += intVar
+	}
+	return result
 }
 
-func getBiggest(elves []string)(int) {
-    result := compute(elves[0])
+func getBiggest(elves []string) int {
+	result := compute(elves[0])
 
-    for _, value := range elves {
-        tmp := compute(value)
-        if (tmp > result) {
-            result = tmp
-        }
-    }
-    return result
+	for _, value := range elves {
+		tmp := compute(value)
+		if tmp > result {
+			result = tmp
+		}
+	}
+	return result
 }
 
 func main() {
-    if (len(os.Args) == 2) {
-        dat, err := os.ReadFile(os.Args[1])
-        check(err)
-        chars := strings.Split(string(dat), "\n\n")
-        if (len(chars) == 0) {
-            fmt.Println("File empty or unvalid")
-        } else {
-            fmt.Println(getBiggest(chars))
-        }
-    } else {
-        fmt.Println("Error, you need to pass a file as argument.")
-    }
+	if len(os.Args) == 2 {
+		dat, err := os.ReadFile(os.Args[1])
+		check(err)
+		chars := strings.Split(string(dat), "\n\n")
+		if len(chars) == 0 {
+			fmt.Println("File empty or unvalid")
+		} else {
+			fmt.Println(getBiggest(chars))
+		}
+	} else {
+		fmt.Println("Error, you need to pass a file as argument.")
+	}
 }
--- day01/second.go
+++ day01/second.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "strconv"
-    "sort"
-    "os"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func compute(elve string)(int) {
-    result := 0
+func compute(elve string) int {
+	result := 0
 
-    for _, value := range strings.Split(elve, "\n") {
-        intVar, err := strconv.Atoi(value)
-        if (err != nil) {
-            fmt.Println(err)
-            return 0
-        }
-        result += intVar
-    }
-    return result
+	for _, value := range strings.Split(elve, "\n") {
+		intVar, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Println(err)
+			return 0
+		}
+		result += intVar
+	}
+	return result
 }
 
-func getThreeBiggest(elves []string)(int) {
-    sort.SliceStable(elves, func(i, j int) bool {
-        return compute(elves[i]) > compute(elves[j])
-    })
+func getThreeBiggest(elves []string) int {
+	sort.SliceStable(elves, func(i, j int) bool {
+		return compute(elves[i]) > compute(elves[j])
+	})
 
-    return compute(elves[0]) + compute(elves[1]) + compute(elves[2])
+	return compute(elves[0]) + compute(elves[1]) + compute(elves[2])
 }
 
 func main() {
-    if (len(os.Args) == 2) {
-        dat, err := os.ReadFile(os.Args[1])
-        check(err)
-        chars := strings.Split(string(dat), "\n\n")
-        if (len(chars) == 0) {
-            fmt.Println("File empty or unvalid")
-        } else {
-            fmt.Println(getThreeBiggest(chars))
-        }
-    } else {
-        fmt.Println("Error, you need to pass a file as argument.")
-    }
+	if len(os.Args) == 2 {
+		dat, err := os.ReadFile(os.Args[1])
+		check(err)
+		chars := strings.Split(string(dat), "\n\n")
+		if len(chars) == 0 {
+			fmt.Println("File empty or unvalid")
+		} else {
+			fmt.Println(getThreeBiggest(chars))
+		}
+	} else {
+		fmt.Println("Error, you need to pass a file as argument.")
+	}
 }
